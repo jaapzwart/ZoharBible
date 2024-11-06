4d79049ae91429cf27c5532c5ddb70d504dbe9b3
jaapzwart <jzwart@msn.com>
2024-11-06 22:47:57 +0100

    Latest

--- obj/Debug/net8.0-android/ZoharBible.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/ZoharBible.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZoharBible")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+09466f24ef076574d3f49bf8190545092babe103")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZoharBible")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZoharBible")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
