09466f24ef076574d3f49bf8190545092babe103
jaapzwart <jzwart@msn.com>
2024-11-06 19:32:24 +0100

    Added comments.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,6 +6,13 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    /// <summary>
+    /// Handles the Click event of the "Get a random Proverb" button.
+    /// Navigates the user to the Proverbs page.
+    /// </summary>
+    /// <param name="sender">The source of the event. Typically, this is the button that was clicked.</param>
+    /// <param name="e">An EventArgs that contains the event data.</param>
     private async void OnGetProverbButtonClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Proverbs());
